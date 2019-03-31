5afd8617d35e821631ea69ce029d4af9faf3f42d
michael tran <michaelt437@gmail.com>
2019-03-31 05:23:05 -0400

    add rating wheel'

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -5,12 +5,26 @@
     <h4 class="mb2">{{movie.tagline}}</h4>
     <p class="mb0"><strong>Synopsis</strong></p>
     <p class="mb2">{{movie.overview}}</p>
-    <dl class="pb2">
-      <dt>Release Date</dt>
-      <dd class="mt0">{{$moment(movie.release_date).format('MMMM D, YYYY')}}</dd>
-      <dt>Runtime</dt>
-      <dd class="mt0">{{movie.runtime || '???'}} minutes</dd>
-    </dl>
+    <div class="columns">
+      <div class="column col-2">
+        <dl class="pb2">
+          <dt>Release Date</dt>
+          <dd class="mt0">{{$moment(movie.release_date).format('MMMM D, YYYY')}}</dd>
+          <dt>Runtime</dt>
+          <dd class="mt0">{{movie.runtime || '???'}} minutes</dd>
+        </dl>
+      </div>
+      <div class="column col-8">
+        <div class="graph_wrapper">
+          <h3 class="vote_average">{{movie.vote_average}}</h3>
+          <GChart
+            type="PieChart"
+            :data="chartData"
+            :options="chartOptions"
+          />
+        </div>
+      </div>
+    </div>
     <div class="columns">
       <div class="column col-4">
         <ul class="list--unstyled">
@@ -40,8 +54,12 @@
 </template>
 
 <script>
+import { GChart } from 'vue-google-charts'
 export default {
   name: 'movie-details',
+  components: {
+    GChart
+  },
   data() {
     return {
       showMoreCast: false,
@@ -56,6 +74,32 @@ export default {
   methods: {
     sortCrew(a, b) {
       return a.job > b.job
+    },
+    setRatingColor(rating) {
+      switch(Math.round(rating)) {
+        case 1:
+          return '#911f00'
+        case 2:
+          return '#f41818'
+        case 3:
+          return '#f9592e'
+        case 4:
+          return '#f9862e'
+        case 5:
+          return '#f6b437'
+        case 6:
+          return '#f6d237'
+        case 7:
+          return '#f4ff4d'
+        case 8:
+          return '#a4ec47'
+        case 9:
+          return '#7ce32d'
+        case 10:
+          return '#15d270'
+        default:
+          return '#666666'
+      }
     }
   },
   computed: {
@@ -85,6 +129,26 @@ export default {
     },
     posterSrc() {
       return this.$store.getters.getPoster;
+    },
+    chartData() {
+      return [
+        ['fill', 'remainder'],
+        ['f', this.movie.vote_average * 10],
+        ['r', 100 - (this.movie.vote_average * 10)]
+      ]
+    },
+    chartOptions() {
+      return {
+        backgroundColor: 'transparent',
+        colors: [this.setRatingColor(this.movie.vote_average), 'transparent'],
+        enableInteractivity: false,
+        legend: 'none',
+        pieSliceBorderColor: 'transparent',
+        pieSliceText: 'none',
+        pieHole: 0.8,
+        height: '105',
+        width: '105'
+      }
     }
   }
 }
